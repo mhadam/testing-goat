fdcdd5e983e322798dade5928198fe827996d972
mhadam <mike@hadam.us>
2016-11-17 19:31:15 -0500

    functional test for removing error box on input

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,7 +1,9 @@
-from unittest import skip
 from .base import FunctionalTest
 
+
 class ItemValidationTest(FunctionalTest):
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
 
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
@@ -30,3 +32,17 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith starts a new list in a way that causes a validation error:
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertTrue(error.is_displayed())
+
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # She is pleased to see that the error message disappears
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import redirect, render
 from lists.models import Item, List
 from lists.forms import ItemForm
-from django.core.exceptions import ValidationError
+# from django.core.exceptions import ValidationError
+
 
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
+
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
@@ -15,6 +17,7 @@ def view_list(request, list_id):
             Item.objects.create(text=request.POST['text'], list=list_)
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, 'form': form})
+
 
 def new_list(request):
     form = ItemForm(data=request.POST)
